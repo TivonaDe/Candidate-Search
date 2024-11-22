dfa52fd3560fc791ee2f60ea412f93a06551d1fb
De vona C <devonacanada@gmail.com>
2024-11-21 22:02:01 -0500

    main edit

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,7 @@ function App() {
   );
 }
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
 
-app.get('/', (_req: any, res: { send: (arg0: string) => void; }) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 export default App;
 
