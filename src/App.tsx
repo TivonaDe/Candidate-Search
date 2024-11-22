7b7b9decc69612e4bcdb484932f28becada02ba8
De vona C <devonacanada@gmail.com>
2024-11-21 21:07:44 -0500

    express added

--- src/App.tsx
+++ src/App.tsx
@@ -12,4 +12,17 @@ function App() {
   );
 }
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (_req: any, res: { send: (arg0: string) => void; }) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 export default App;
+
